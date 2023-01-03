edf7c4813060e3817eaa0534004cd878a01240fa
Daniel <dacamoty@gmail.com>
2023-01-03 16:38:57 -0500

    icons

--- components/atom/Icon/styled.ts
+++ components/atom/Icon/styled.ts
@@ -1,5 +1,12 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    
-`
+  width: 100%;
+  .st0 {
+    fill: none;
+    stroke-width: 2;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-miterlimit: 10;
+  }
+`;
